8d5756b0cf86828245b0b81d0408bf9330ed9243
DiegoAGtz <diegogtz1207@gmail.com>
2025-03-24 22:59:07 -0600

    feat: true message

--- hello-world/app.ts
+++ hello-world/app.ts
@@ -15,7 +15,7 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
         return {
             statusCode: 200,
             body: JSON.stringify({
-                message: 'hello world, this is a change from master',
+                message: 'Forget about the hello world stuf, this is the real change',
             }),
         };
     } catch (err) {
